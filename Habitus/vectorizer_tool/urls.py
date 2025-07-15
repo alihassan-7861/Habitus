4443a92d3aca48cdff870c0c906d309267fa15dc
alihassan-7861 <malikalihassankhan786@gmail.com>
2025-07-15 20:29:04 +0500

    Update urls.py

--- Habitus/vectorizer_tool/urls.py
+++ Habitus/vectorizer_tool/urls.py
@@ -5,7 +5,7 @@ from .views import vectorizer_form_view,VectorizeImageView,PaintByNumberView,Mul
 
 
 urlpatterns = [
-    path('vectorizer-ui/', vectorizer_form_view, name='vectorizer_ui'),
+    path('', vectorizer_form_view, name='vectorizer_ui'),
     path('vectorize/',  VectorizeImageView.as_view(), name='vectorize'),
     path('test-pbn/', views.test_pbn_frontend, name='test_pbn_frontend'),
     path("generate-pbn/", PaintByNumberView.as_view(), name="generate_pbn"),
